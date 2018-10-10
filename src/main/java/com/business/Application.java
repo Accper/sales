941f1d98025db539fb0d7cfb2a69a2cf4673f623
hi-yamy <577300012@qq.com>
2018-10-10 21:11:53 +0800

    system.out.println

--- src/main/java/com/business/Application.java
+++ src/main/java/com/business/Application.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("sales");
 		SpringApplication.run(Application.class, args);
 	}
 }
